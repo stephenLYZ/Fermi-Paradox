a42d4849f5477e552aa8f5db5215577bdd1232ca
stephenliu <stephenlyao@yeah.net>
2016-10-23 17:27:52 +0800

    first commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * https://HosseinKarami.github.io/fastshell
+ * @author Hossein Karami
+ * @version 1.0.5
+ * Copyright 2016. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+})(jQuery, window, document);
